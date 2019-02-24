96b4b24e53f82cfbaf603f7fd8618351b918b930
Spencer Smith <stix1919@hotmail.com>
2019-02-24 15:07:07 -0800

    Mostly done with Dynamic. Need to finish up any sort for units. look into testing

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const {json} = require('body-parser')
 const axios = require('axios')
 const cors = require('cors')
 
+let units = []
+
 const port = 3001
 
 const app = express()
@@ -11,9 +13,12 @@ app.use(json())
 app.use(cors())
 
 app.get('/api/getUnits', (req, res) => {
-    axios.get('https://age-of-empires-2-api.herokuapp.com//api/v1/units')
+    if(units[0]){
+        res.status(200).send(units)
+    } else  
+        axios.get('https://age-of-empires-2-api.herokuapp.com//api/v1/units')
         .then(response => {
-            console.log('hit')
+            units = response.data
             return res.status(200).send(response.data)
         }).catch(err => console.log(err))
 })
--- src/views/Dynamic.vue
+++ src/views/Dynamic.vue
@@ -27,7 +27,41 @@
       </div>
       <button @click='sortAllUnits'>Calculate</button>
 
-      <div></div>
+      <div class='sorted-units'>
+        <div class="unit-box" 
+              v-for="(unit, index) in sortedUnits" 
+              :key="index"
+              >
+          <h3 class='unit-name'>{{unit.name}}</h3>
+          <h5>Hit Points: {{unit.hit_points}}</h5>
+          <h5>Build Time: {{unit.build_time}}</h5>
+          <h5>Gold: {{unit.cost.Gold}}</h5>
+        </div>
+      </div>
+      <div>
+        <h3>5 Best Units</h3>
+        <div class='checkBoxes'>
+          <h5>Same Units</h5>
+          <input type='checkbox' :checked="anyCheck === false" @click="updateCheck"/>
+          <h5>Any Units (WIP)</h5>
+          <input type='checkbox' :checked="anyCheck === true" @click="updateCheck"/>
+        </div>
+      </div>
+
+
+      <div class='sorted-units'>
+        <div class="unit-box" 
+              v-for="(unit, index) in bestUnits" 
+              :key="index"
+              >
+          <h3 class='unit-name'>{{unit.name}}</h3>
+          <h5>Hit Points: {{unit.hit_points}}</h5>
+          <h5>Build Time: {{unit.build_time}}</h5>
+          <h5>Gold: {{unit.cost.Gold}}</h5>
+        </div>
+      </div>
+
+
     </div>
   </div>
 </template>
@@ -45,9 +79,11 @@ export default {
       btMore: false,
       cstInput: 0,
       cstMore: false,
+      anyCheck: true,
 
       units: [],
-      sortedUnits: []
+      sortedUnits: [],
+      bestUnits: []
     }
   },
   methods: {
@@ -55,33 +91,59 @@ export default {
       this[name] = !this[name]
     },
     sortAllUnits: function(){
-      console.log(this.hpInput, -this.hpInput)
       this.sortedUnits = this.units.filter((unit, index) => {
-        console.log(unit.hit_points > this.hpInput)
         //hp Filter
         if(this.hpMore === true){
           return unit.hit_points > this.hpInput
         } else return unit.hit_points < this.hpInput
       }).filter((unit, index) => {
         //build filter
-        console.log(unit.build_time > this.btInput)
         if(this.btMore === true){
           return unit.build_time > this.btInput
         } else return unit.build_time < this.btInput
       }).filter((unit, index) => {
         //cost filter
-        console.log(unit.cost.Gold)
         if(this.cstMore === true){
+          
           return unit.cost.Gold > this.cstInput
-        } else return unit.cost.Gold < this.cstInput
-      })
-
+        } else if(this.cstMore === false){
+          if(unit.cost.Gold === undefined){
+              return true
+          }
+          return unit.cost.Gold < this.cstInput
+        }
+      }).sort((a, b) => a.cost.Gold - b.cost.Gold)
+      this.bestFive()
       console.log(this.sortedUnits)
+    },
+    bestFive: function(){
+      let finalFive = []
+      if(this.anyCheck === false){
+
+        let filteredUnits = this.sortedUnits
+          .sort((a, b) => a.build_time - b.build_time)
+          .filter((unit, index, arr) => {
+            return unit.build_time === arr[0].build_time
+          })
+          .sort((a, b) => b.hit_points - a.hit_points)
+
+        for(let i = 0; i < 5; i++){
+          finalFive.push(filteredUnits[0])
+        }
+        console.log(filteredUnits) 
+        this.bestUnits = finalFive
+        // this.bestUnits = filteredUnits
+      }
+      else {
+
+      }
+    },
+    updateCheck: function() {
+      this.anyCheck = !this.anyCheck
     },
 
     getUnits: function(){
       axios.get('http://localhost:3001/api/getUnits').then(response => {
-        console.log(response.data.units, 'inside')
         this.units = response.data.units
       }).catch(err => console.log(err, 'broke'))
     },
@@ -95,8 +157,40 @@ export default {
 
 <style>
 
-.filter-inputs {
-  display: flex
-}
+  .filter-inputs {
+    display: flex
+  }
+
+  .sorted-units {
+    height: 240px;
+    width: 700px;
+
+    /* border: solid black 2px; */
+    display: flex;
+    overflow-x: auto;
+
+    box-shadow: inset 0px 0px 15px 5px
+
+    /* overflow-x: scroll;
+    overflow-y: hidden;
+    white-space: nowrap; */
+  }
+
+  .unit-box {
+    height: 200px;
+    min-width: 150px;
+    margin: 10px;
+
+    border: solid black 2px;
+    /* display: inline-block; */
+  }
+  .unit-name {
+    height: 60px;
+    max-width: 100%;
+  }
+
+  .checkBoxes {
+    display: flex
+  }
 
 </style>
